a24c41be8f6b3e13ff2ca336bb40cf1f183b8f22
lokreanand <32187019+lokreanand@users.noreply.github.com>
2022-05-11 17:15:10 +0530

    testing

--- app/src/main/java/com/example/iconfinder_app/MainActivity.kt
+++ app/src/main/java/com/example/iconfinder_app/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.iconfinder_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var num=10
+        Toast.makeText(this, num, Toast.LENGTH_SHORT).show()
     }
+
 }
